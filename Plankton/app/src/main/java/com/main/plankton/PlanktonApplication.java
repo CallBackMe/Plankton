845e733c1a917651ff9550354e38b9492329a696
Dylan <zhaohaiyang@xdf.cn>
2016-09-28 16:25:49 +0800

    plankton base line

---
+++ Plankton/app/src/main/java/com/main/plankton/PlanktonApplication.java
@@ -0,0 +1,17 @@
+package com.main.plankton;
+
+import android.app.Application;
+
+/**
+ * Created by ocean.z on 16/9/28.
+ */
+
+public class PlanktonApplication extends Application{
+
+	private static final String TAG = "PlanktonApplication";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
